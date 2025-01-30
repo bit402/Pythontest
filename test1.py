bac95fe4a38d86c5ff05402a627310b83733b5b6
bit402 <1160092090@qq.com>
2025-01-30 22:31:14 +0800

    测试命令提交

---
+++ test1.py
@@ -0,0 +1,60 @@
+#print("Hello, World!")
+#print("Hello,world")
+#print(20)
+#print('Hello,Python')
+#a=100
+#b=50
+#print(90)
+#print(a*b)
+#print('你好北京！')
+#print("你好北京！")
+#print('''你好北京！''')
+#print("""你好北京！""")
+#a=50
+#b=100
+#print(a,b,"要么出众，要么出局")
+#print('b')
+#print(chr(98))
+#print('C')
+#print(chr(67))
+# print(8)
+# print(chr(56))
+# print('[')
+# print(chr(91))
+# print(ord('北'))
+# print(ord('京'))
+# print(chr(21271))
+# print(chr(20140))
+# fp=open("note.txt","w")
+# print("北京欢迎你！",file=fp)
+# fp.close()
+# print("北京",end="-->")
+# print("欢迎您！")
+"""print("北京欢迎你"+"2023")
+a=2023
+print("北京欢迎你"+a)
+name=input("请输入您的姓名：")
+print("我的姓名是："+name)"""
+'''num=input("请输入您的幸运数字：")
+print("我的幸运数字是："+num)
+num=(int(num))
+print("您的幸运数字是：",num)'''
+# print("hello")
+# print("world")
+# #类的定义
+# class student:
+#     pass
+# #函数的定义
+# def fun():
+#     pass
+#fp=open("test.txt","w")
+# fp=open("text.txt","w")
+# print("人生苦短，我用Python",file=fp)
+# fp.close()
+name=input("请输入您的姓名：")
+age=input("请输入您的年龄")
+zuo=input("请输入您的座右铭")
+print("--------自我介绍------")
+print("姓名："+name)
+print("年龄："+age)
+print("座右铭"+zuo)
