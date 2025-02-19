46978055bf4d3b21f41f544bcb2b641a0d25bb25
bit402 <1160092090@qq.com>
2025-02-19 19:40:55 +0800

    第二次练习

---
+++ test2.py
@@ -0,0 +1,77 @@
+# luck_number=8
+# my_name="张三"
+# print("luck_number的数据类型是：",type(luck_number))
+# print("my_name的数据类型是：",type(my_name))
+# print(my_name,"的幸运数字是：",luck_number)
+# luck_number="北京欢迎你！"
+# print("luck_number的数据类型是：",type(luck_number))
+# no=number=1024#no和number都指向了1024这个整数值
+# print(no,number)
+# print(id(no))#id()查看对象内存地址的
+# print(id(number))
+# a=1024
+# print(id(a))
+# pi=3.1415926#定义了一个常量
+# PI=3.1415926#定义了一个变量
+# num=987
+# num2=0b1010101
+# num3=0o765
+# num4=0x87abf
+# print(num)
+# print(num2)
+# print(num3)
+# print(num4)
+# height=187.6#身高
+# print(height)
+# print(type(height))#type()查看height这个变量的数据类型
+# x=10
+# y=10.0
+# print(x,y)
+# print("x的数据类型为：",type(x))
+# print("y的数据类型为：",type(y))
+# x=1.99E1413
+# print("科学计数法：",x,"x的数据类型为：",type(x))
+# print(0.1+0.2)
+# print(round(0.1+0.2,1))
+# x=123+456j
+# print("x的实数部分：",x.real,"x的虚数部分：",x.imag)
+# city="天津"
+# address="天津市宝坻区香江大街3号"
+# print(city)
+# print(address)
+# info="""地址：天津市宝坻区香江大街3号
+#         收件人：张三
+#         电话：18600000000"""
+# info2='''地址：天津市宝坻区香江大街3号
+#         收件人：张三
+#         电话：18600000000'''
+# print(info)
+# print("-------------------------------")
+# print(info2)
+# print("北京")
+# print("欢迎你")
+# print("-----------------------------------")
+# print("北京\n欢迎你")
+# print("北京北京\t欢迎你")
+# print("helloooo\to")
+# print("hellooooooooooooooo")
+# print("老师说：\"好好学习，天天向上\"")
+# print("老师说：\'好好学习，天天向上\'")
+# print("北\n京\n欢\n迎\n你")
+# print(r"北\n京\n欢\n迎\n你")
+# print(R"北\n京\n欢\n迎\n你")
+# s="HELLOWORLD"
+# print("北京欢迎你"[4])
+# print("北京欢迎你"[-1])
+# print("------------------")
+# print(s[2:7])
+# print(s[-8:-3])
+# print(s[:5])
+# print(s[5:])
+x="2022年"
+y="北京冬奥会"
+print(x+y)
+print(x*10)
+print(10*x)
+print("北京" in y)
+print("上海" in y)
