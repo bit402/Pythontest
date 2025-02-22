42e8541a5716e9e43d275048e8228908e3f4259d
bit402 <1160092090@qq.com>
2025-02-22 11:19:26 +0800

    练习4

---
+++ test4.py
@@ -0,0 +1,48 @@
+# s="3.14+3"
+# print(s,type(s))
+# x=eval(s)
+# print(x,type(x))
+# age=eval(input("请输入您的年龄："))
+# print(age,type(age))
+# height=eval(input("请输入您的身高："))
+# print(height,type(height))
+# hello="北京欢迎你"
+# print(hello,type(hello))
+# print(eval("hello"))
+# print(eval("北京欢迎你"))#程序报错
+# print("加法：",1+1)
+# print("减法：",1-1)
+# print("乘法：",2*3)
+# print("除法：",10/2)
+# print("整除：",10//3)
+# print("取余：",10%3)
+# print("幂运算：",2**4)
+# print(0/2)
+# print(2/0)#程序报错
+#python支持链式赋值
+# a=b=c=100
+# print(a,b,c)
+# #python支持系列解包赋值
+# a,b=10,20
+# print(a,b,c)
+# print("------如何交换两个变量的值呢？-------")
+# a,b=b,a
+# print(a,b,c)
+# print(8>7 and 6>5)
+# print(type(8>7 and 6>5))
+# print(8<7 and 10/0)
+# print(8>7 or 10/0)
+# print(not True)
+# print(not False)
+# print(not(8>7))
+# print("按位与运算：",12 & 8)
+# print(8&12)
+# print("按位或运算：",4|8)
+# print(8|4)
+# print("按位异或运算符：",31^32)
+# print(32^31)
+# print("按位取反：",~123)
+print("左移位：",2<<2)
+print("左移位：",2<<3)
+print("左移位：",2<<4)
+print("左移位：",32<<1)
