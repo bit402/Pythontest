1704b76833d4e42323b2c2b4760367034736422d
bit402 <1160092090@qq.com>
2025-11-02 12:25:03 +0800

    练习

---
+++ test17.py
@@ -0,0 +1,61 @@
+# for i in range(4):
+#     for j in range(4):
+#         print("*",end="")
+#     print()
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+# for i in range(5):
+#     for j in range(5-i):
+#         print("*",end="")
+#     print()
+# print("haha\nhaha")
+# print("haha\thaha")
+# print("haha\\haha")
+# print("haha\'hehehehe\'")
+# print("haha\"h")
+# s1="hello"
+# s2=s1
+# s3="hello"
+# s4="hello1"
+# print(s1,s2,s3,s4)
+# print(id(s1),id(s2),id(s3),id(s4))
+# print(s1 is s2)
+# print(s1 is s3)
+# print(s1 is s4)
+# s1="world"
+# print(s1,s2,s3,s4)
+#s1="ABCDEFG"
+# print(s1[4])
+# print(s1[6],s1[-1])
+# print(s1[0:4])
+# print(s1[0:5])
+# print(s1[-3:])
+# print(s1[-3:7])
+# print(s1[:])
+# print(s1)
+# print(id(s1[:]),id(s1))
+# print(s1[:] is s1)
+# print(type(s1[:] is s1))
+# print(s1[1:-1])
+# print(s1[2:-2])
+# print(s1[:-1:2])
+# print(s1[1::2])
+# print(s1[::4])
+# print(s1[-3::-4])
+# print(s1[3:1:-1])
+path="www.baidu.comb"
+# i=path.find("b")
+# print(i)
+# i=path.rfind("b")
+# print(i)
+# i=path.count("b")
+# print(i)
+# i=path.index("#")
+# i=path.find("baidu")
+# print(i)
+result=path.startswith("www.")
+print(result)
+result=path.endswith(".com")
+print(result)
