4037c858b1998d903ce8b42001e822187762db93
bit402 <1160092090@qq.com>
2025-11-13 16:49:33 +0800

    练习

---
+++ test21.py
@@ -0,0 +1,36 @@
+# sppech = "昨天晚上遇到了一个漂亮的小姐姐，要不要表白呢？"
+# print(sppech)
+# user_name = input("请输入用户名:")
+# while True:
+#     reply = input("请输入回复内容：")
+#     if (len(reply.strip()) == 0):
+#         print("对不起回复内容不能为空！")
+#     elif (len(reply.strip()) > 20):
+#         print("对不起，评论不能超过20个字！")
+#     elif (reply.find("滚") or reply.find("傻")):
+#         reply=reply.replace("滚",'*')
+#         reply=reply.replace("傻","*")
+#         #print(reply)
+#         break
+#     elif(reply.startswith(" ")or reply.endswith(" ")):
+#         reply=reply.strip()
+#         #print(reply)
+#         break
+# print(user_name+":")
+# print("        "+reply)
+# print("小黑：")
+# print("       .......")
+# print("小花：")
+# print("       。。。。")
+# s="傻了吧"
+# print(s.find("傻"))
+# result=s.replace("傻",'*')
+# print(result)
+lst=[]
+print(type(lst))
+lis2=["牛奶","面包","火腿肠","辣条","臭豆腐","饼干"]
+print(lis2)
+print(lis2[lis2.index("辣条")])
+print(lis2[0])
+print(lis2[0:2])
+print(lis2[2:4])
